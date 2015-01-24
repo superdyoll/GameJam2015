8ae38e63fb1c3125279bffa4874503b1b4db890b
unknown <meja1g14@korma.ecs.soton.ac.uk>
2015-01-24 11:13:23 +0000

    Fix Dino, organisation

---
+++ Dinosaurs/Dino.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public abstract class Dino {
+
+	abstract protected int baseHealth        { get; }
+	abstract protected int baseSpeed         { get; }
+	abstract protected int baseSurvivability { get; }
+	abstract protected int baseExplosive     { get; }
+	abstract protected int baseExp           { get; }
+
+	public int health { get; set; }
+	public int speed { get; set;}
+	public int survivability { get; set; }
+	public int explosive { get; set; }
+	public int exp { get; set; }
+
+	public Boolean playerControlled{ get; set; }
+
+	int level { get; set; }
+
+	public Dino() {
+		int gameLevel = 1;
+		
+		System.Random rnd = new System.Random ();
+		int rndLevel = rnd.Next (-2, 2);
+		int boss = rnd.Next (20);
+		
+		level = gameLevel + rndLevel;
+		
+		health 			= (int) Math.Pow (baseHealth, level);
+		speed 			= (int) Math.Pow (baseSpeed, level);
+		survivability 	= (int) Math.Pow (baseSurvivability, level);
+		explosive 		= (int) Math.Pow (baseExplosive, level);
+		exp 			= (int) Math.Pow (baseExp, level);
+	}
+
+	// Use this for initialization
+	abstract protected void Start ();
+	
+	// Update is called once per frame
+	abstract protected void Update ();
+}
---
+++ Weapons/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	abstract protected int baseDamage { get; }
+	abstract protected int baseRadius { get; }
+	abstract protected int baseRange  { get; }
+	
+	public int damage { get; set; }
+	public int radius { get; set; }
+	public int range  { get; set; }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
