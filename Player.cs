39ae1c547036387a321b0dd69c757e8a56788049
unknown <sw3g14@tikka.ecs.soton.ac.uk>
2015-01-24 10:27:24 +0000

    Updated camera follow and look at mouse direction

--- Player.cs
+++ Player.cs
@@ -6,6 +6,24 @@ public class Player : MonoBehaviour
 	public float speed = 1;
 
 	void Update () {
+		GetMovementInput ();
+		GetMouseInput ();
+		UpdateCameraPosition ();
+	}
+
+	private void UpdateCameraPosition(){
+		Camera.main.transform.position = new Vector3 (transform.position.x, transform.position.y, -20f);
+	}
+
+	private void GetMouseInput(){
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x - Screen.width / 2f, Input.mousePosition.y - Screen.height / 2f, 0f);
+		Debug.Log (mousePosition);
+		mousePosition.Normalize ();
+		float rotZ = Mathf.Atan2 (mousePosition.y, mousePosition.x) * Mathf.Rad2Deg; // find the angle in degrees
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + 90f);
+	}
+
+	private void GetMovementInput(){
 		if (Input.GetKey ("w")) {
 			if(gameObject.transform.position.y < 50f){
 				gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 0.1f * speed);
